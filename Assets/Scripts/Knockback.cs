94eabe918a4d4ce2166fcb26834768fbcf62e495
VeronicaFiori <ver.fiori1@stud.uniroma3.it>
2024-01-19 22:12:13 +0100

    allontanamento nemico

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+
+    public float thrust;
+    public float knockTime;
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if (enemy != null)
+            {
+                enemy.GetComponent<Nemico>().currentState= EnemyState.stagger ;                
+                Vector2 difference = enemy.transform.position - transform.position;
+                difference = difference.normalized * thrust;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemy));
+                Debug.Log("coroutine");
+
+            }
+        }
+                
+            
+        }
+
+        private IEnumerator KnockCo(Rigidbody2D enemy)
+        {
+
+            if (enemy != null)
+            {
+              
+                yield return new WaitForSeconds(knockTime);
+                enemy.velocity = Vector2.zero;
+            enemy.GetComponent<Nemico>().currentState = EnemyState.idle;
+            }
+        }
+}
